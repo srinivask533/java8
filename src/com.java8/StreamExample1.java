888e16c6c1ae79ce657aa179905c31ea35c89d02
ksrinivas <katukojwalsrinivas@yahoo.com>
2025-04-01 23:48:16 +0530

    added src folder

--- src/com.java8/StreamExample1.java
+++ src/com.java8/StreamExample1.java
@@ -1,6 +1,11 @@
 import java.utiil.*;
 public class StreamExample1{
-    public static void main(String[] args){
+    public static void main(String args[]){
        System.out.println("Hell");
+    }
+
+    public void printList(){
+        List<String> cityList = List.of("Hyderbad","Banglore","Chennai");
+        cityList.stream().foreach(System.out::println);
     }
 }
